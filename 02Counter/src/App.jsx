a7c35735e04ab524e42ea7d5fb9cba6d0f03e5ce
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-08-07 15:53:27 +0530

    Refactor counter increment logic to use functional updates for state management

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -5,11 +5,29 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [counter, setCounter] = useState(15)
 
   const addValue = () => {
     if(counter < 20){
-    setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1) // agar hum soche ki iss tarah se ek bar me 4 increment karenge to ye kaam nahi karega kyuki react me state update asynchronous hota hai aur ye previous state ke basis pe hi update hota hai. isliye agar hum ek hi function me multiple times setCounter call karenge to wo sirf last wale call ko consider karega aur pehle wale calls ko ignore karega. isliye agar hume ek hi function me multiple times state update karna hai to hume functional update ka use karna chahiye jisme hum previous state ko access karke new state calculate karte hai. jaise ki niche diya gaya hai.
+
+
+    setCounter((prevCounter) => {
+      let counter = prevCounter + 1
+      return counter
+
+    })
+    setCounter((prevCounter) => {return prevCounter + 1}) //short form of above code.
+    setCounter((prevCounter) => prevCounter + 1) // 2nd short form of above code.
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+
+    //the reason is react is tarah ke same operations ko batch kar deta hai aur ek hi state update ke liye multiple times call karne se wo sirf last wale call ko consider karega aur pehle wale calls ko ignore karega. isliye agar hume ek hi function me multiple times state update karna hai to hume functional update ka use karna chahiye jisme hum previous state ko access karke new state calculate karte hai. jaise ki niche diya gaya hai.
+
     }
   }
 
