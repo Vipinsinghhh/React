95a9dc457b443741734c87f07c1f4997c7b5b6db
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-09-01 19:04:27 +0530

    refactor AuthService methods to use class syntax for better readability

--- 12MegaBlogApp/src/appwrite/auth.js
+++ 12MegaBlogApp/src/appwrite/auth.js
@@ -12,44 +12,44 @@ export class AuthService {
             .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client)
     }
-}
 
-async function createAccount({email, password, name}) {
-    try {
-        const userAccount = await this.account.create(ID.unique(), email, password, name);
-        if (userAccount) {
-            // call another method
-            return this.login({email, password});
-        } else {
-            return userAccount;
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login({ email, password });
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
         }
-    } catch (error) {
-        throw error;
     }
-}
 
-async function login({email, password}) {
-    try {
-        return await this.account.createEmailSession(email, password);
-    } catch (error) {
-        throw error;
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
     }
-}
 
-async function getCurrentUser() {
-    try {
-        return await this.account.get();
-    } catch (error) {
-        console.log("Appwrite serive :: getCurrentUser :: error", error);
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+        return null;
     }
-    return null;
-}
 
-async function logout() {
-    try {
-        await this.account.deleteSessions();
-    } catch (error) {
-        console.log("Appwrite serive :: logout :: error", error);
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
     }
 }
 
