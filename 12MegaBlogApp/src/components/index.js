c58d01ecc014396f267ceddc24d7fc12cb20aaa8
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-09-06 00:39:39 +0530

    added RTE and Signup components to index export and corrected Logo import case

--- 12MegaBlogApp/src/components/index.js
+++ 12MegaBlogApp/src/components/index.js
@@ -1,13 +1,17 @@
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
 import Container from "./container/Container.jsx";
-import Logo from "./Logo.jsx";
+import Logo from "./logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE from "./RTE.jsx";
+import Signup from "./Signup.jsx";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup
 }
