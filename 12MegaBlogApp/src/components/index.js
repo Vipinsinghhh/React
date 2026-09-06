e1cbee5129fa1a573b839581d89d4a8f25376cba
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2026-09-07 00:22:39 +0530

    refactored index export to include Login, Button, PostCard, AuthLayout, and Input components

--- 12MegaBlogApp/src/components/index.js
+++ 12MegaBlogApp/src/components/index.js
@@ -1,10 +1,18 @@
-import Header from "./Header/Header.jsx";
-import Footer from "./Footer/Footer.jsx";
-import Container from "./container/Container.jsx";
-import Logo from "./logo.jsx";
-import LogoutBtn from "./Header/LogoutBtn.jsx";
-import RTE from "./RTE.jsx";
-import Signup from "./Signup.jsx";
+import Select from "./Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+
 
 export {
     Header,
@@ -13,5 +21,12 @@ export {
     Logo,
     LogoutBtn,
     RTE,
-    Signup
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
+    Input,
+    Select,
 }
